8f2ce658ff08d357f0e991fa112faee65b650ee1
NotableDeveloper <castlereal01@gmail.com>
2023-12-19 22:31:47 +0900

    chore : 프로젝트 구조 변경
    - 다수의 컨테이너로 서비스를 구동하기 위해 구조 변경
    - 기존의 application은 codelab_app이라는 이름으로 변경
    - STOMP 기반의 MessageBroker application은 message_broker라는 디렉터리에 저장

---
+++ codelab_app/src/main/java/com/example/CodeLab/CodeLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodeLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeLabApplication.class, args);
+	}
+
+}
--- message_broker/src/test/java/com/example/MessageBroker/MessageBrokerApplicationTests.java
+++ message_broker/src/test/java/com/example/MessageBroker/MessageBrokerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.CodeLab;
+package com.example.MessageBroker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CodeLabApplicationTests {
+class MessageBrokerApplicationTests {
 
 	@Test
 	void contextLoads() {
